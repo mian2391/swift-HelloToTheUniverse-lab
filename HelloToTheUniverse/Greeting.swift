5051486a147cc3b5e9434ac67ac583614158dfc9
mian2391 <miananswer@hotmail.com>
2017-04-15 00:37:54 -0400

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,8 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("My name is Woody and I have a snake in my boot")
         return "Hello Universe!"
     }
     
